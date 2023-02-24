6226ca3bb2fae8ca8591058fb00875b30c2d60b8
Christiaan Vink <Christiaan_Vink@outlook.com>
2023-02-25 00:16:25 +0100

    added sletknap , comments and skuffed css stylesheet ( det skal laves pænere :) )

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 import { checkoutlist } from './Basket';
+import './stylesheets/checkouttable.css'; // import CSS styles
 function List() {
-    const [counts, setCounts] = useState(new Array(checkoutlist.length).fill(0));
 
+
+    // Initialize 'counts' state with 0 for each item in the basket.js
+    const [counts, setCounts] = useState(new Array(checkoutlist.length).fill(0));
+    // initialize a sletknap state which is an empty array --.
+    const [sletKnap, setSletKnap] = useState([]);
+    // Adds count button +/- and function
     function Counter({index }) {
         const count = counts[index];
         const clickHandlerIncrement = () => {
@@ -23,8 +29,18 @@ function List() {
         );
     }
 
+
+    // creates the table
     const listItems = checkoutlist.map((item, index) => {
+        // we remove the row if the index of the row is in the removed array.
+        if (sletKnap.includes(index)) return null;
+        // here we calculate the total price..
         const total = item.price * counts[index];
+        // handling the clicks for removing the item. -> (adds index to of the item in the array initialized earlier.
+        const clickHandlerRemove = () => {
+            setSletKnap((removed) => [...removed, index]);
+        };
+        // 1 row with every id the item has. + slet knap (item.d/pice/count/total)
         return (
             <tr key={item.id}>
                 <td>{item.id}</td>
@@ -39,17 +55,25 @@ function List() {
                     {total}
                     {item.currency}
                 </td>
+
+                <td>
+                    <button onClick={clickHandlerRemove}>SLET</button>
+                </td>
+
+
             </tr>
         );
     });
 
+    // calculates the total price of all the items which are not in the sletknap array.
     let totalSum = 0;
     for (let i = 0; i < checkoutlist.length; i++) {
+        if (sletKnap.includes(i)) continue;
         const item = checkoutlist[i];
         const count = counts[i];
         totalSum += item.price * count;
     }
-
+    // Renders the headers, body, and bottom row.
     return (
         <table>
             <thead>
@@ -58,13 +82,14 @@ function List() {
                 <td>Price</td>
                 <td>Quantity</td>
                 <td>Total</td>
+                <td>Slet</td>
             </tr>
             </thead>
             <tbody>
             {listItems}
-            <tr>
+            <tr className="rows-css">
 
-                <td colSpan={3}>Total price:</td>
+                <td colSpan={4}>Total price:</td>
                 <td>
                     {totalSum}
                     {checkoutlist[0].currency}
@@ -82,10 +107,7 @@ function App() {
                 <h1>Checkout</h1>
                 <List/>
             </div>
-
-
         </>
-
     );
 }
 
