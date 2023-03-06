e99eb1c84739d5056765ea6dd6f0237a95c281dd
Gfauser99 <s205446@student.dtu.dk>
2023-03-06 14:48:10 +0100

    UI Fixed to only english

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { checkoutlist } from './Basket';
 import './stylesheets/styles.css'; // import CSS styles
 function List() {
 
-
     // Initialize 'counts' state with 0 for each item in the basket.js
-    const [counts, setCounts] = useState(new Array(checkoutlist.length).fill(0));
-    // initialize a sletknap state which is an empty array --.
-    const [sletKnap, setSletKnap] = useState([]);
+    const [counts, setCounts] = useState(new Array(checkoutlist.length).fill(1));
+    // initialize a deleteButton state which is an empty array --.
+    const [deleteButton, setDeleteButton] = useState([]);
     // Adds count button +/- and function
     function Counter({index }) {
         const count = counts[index];
@@ -33,17 +32,17 @@ function List() {
     }
 
 
-    // creates the table
+    // creates the table-body
     const listItems = checkoutlist.map((item, index) => {
         // we remove the row if the index of the row is in the removed array.
-        if (sletKnap.includes(index)) return null;
+        if (deleteButton.includes(index)) return null;
         // here we calculate the total price.
         const total = item.price * counts[index];
         // handling the clicks for removing the item. -> adds index to of the item in the array initialized earlier.
         const clickHandlerRemove = () => {
-            setSletKnap((removed) => [...removed, index]);
+            setDeleteButton((removed) => [...removed, index]);
         };
-        // 1 row with every id the item has. + slet knap (item.d/pice/count/total)
+        // 1 row with every id the item has. + deleteButton (item.d/pice/count/total)
         return (
             <tr key={item.id}>
                 <td>{item.id}</td>
@@ -58,7 +57,7 @@ function List() {
                 </td>
 
                 <td>
-                    <button onClick={clickHandlerRemove}>SLET</button>
+                    <button onClick={clickHandlerRemove}>Remove</button>
                 </td>
 
 
@@ -69,7 +68,7 @@ function List() {
     // calculates the total price of all the items which are not in the sletknap array.
     let totalSum = 0;
     for (let i = 0; i < checkoutlist.length; i++) {
-        if (sletKnap.includes(i)) continue;
+        if (deleteButton.includes(i)) continue;
         const item = checkoutlist[i];
         const count = counts[i];
         totalSum += item.price * count;
@@ -99,6 +98,69 @@ function List() {
         </table>
     );
 }
+/*
+function AllItems(){
+    const listItems = checkoutlist.map((item, index) => {
+       const id = item.id
+
+    return (
+        <ul>
+            <li>
+                {id} <button>+</button>
+            </li>
+        </ul>
+
+    );
+    });
+    return(
+
+    listItems);
+}
+
+function CheckoutForm(){
+
+    return(
+        <section>
+            <h2>Payment information</h2>
+            <p>
+                <label htmlFor="card">
+                    <span>Card type:</span>
+                </label>
+                <select id="card" name="usercard">
+                    <option value="visa">Visa</option>
+                    <option value="mc">Mastercard</option>
+                    <option value="amex">American Express</option>
+                </select>
+            </p>
+            <p>
+                <label htmlFor="number">
+                    <span>Card number:</span>
+                    <strong><span aria-label="required">*</span></strong>
+                </label>
+                <input type="tel" id="number" name="cardnumber"/>
+            </p>
+            <p>
+                <label htmlFor="expiration">
+                    <span>Expiration date:</span>
+                    <strong><span aria-label="required">*</span></strong>
+                </label>
+                <input
+                    type="text"
+                    id="expiration"
+                    required="true"
+                    placeholder="MM/YY"
+                    pattern="^(0[1-9]|1[0-2])\/([0-9]{2})$"/>
+            </p>
+        </section>);
+
+}
+
+BELONGS IN  App() --------
+ <div className="myDiv">
+            <AllItems/>
+        </div>
+--------------------------
+ */
 
 function App() {
     return (
@@ -108,6 +170,8 @@ function App() {
 
                 <List/>
             </div>
+
+
         </>
     );
 }
